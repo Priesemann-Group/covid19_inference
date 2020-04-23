d00c99602fcd693c48db17f32f5209df8ea838e8
Paul@Alublech <sitzkringel@web.de>
2020-04-23 18:02:50 +0200

    trying to isolate theano failing travis test

--- tests/test_dev_helper.py
+++ tests/test_dev_helper.py
@@ -2,11 +2,13 @@
 # @Author:        F. Paul Spitzner
 # @Email:         paul.spitzner@ds.mpg.de
 # @Created:       2020-04-23 14:43:08
-# @Last Modified: 2020-04-23 17:22:17
+# @Last Modified: 2020-04-23 17:43:58
 # ------------------------------------------------------------------------------ #
 
 def test_dev_helper():
     import pymc3 as pm
+    import theano
+    import theano.tensor as tt
     import covid19_inference as cov
 
     model, trace = cov.create_example_instance()
