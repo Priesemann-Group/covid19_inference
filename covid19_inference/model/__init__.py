90d9670d2f32586182a1d217c7a7aea1b4357af0
pSpitzner <paul.spitzner@ds.mpg.de>
2020-05-19 12:10:15 +0200

    added task delegation to module init for now

--- covid19_inference/model/__init__.py
+++ covid19_inference/model/__init__.py
@@ -2,5 +2,45 @@
 # @Author:        F. Paul Spitzner
 # @Email:         paul.spitzner@ds.mpg.de
 # @Created:       2020-05-19 11:57:28
-# @Last Modified: 2020-05-19 12:03:02
+# @Last Modified: 2020-05-19 12:09:42
 # ------------------------------------------------------------------------------ #
+
+"""
+# TODO 2020-05-19
+
+* pip?
+* automated ipynb execution new repo
+    + script that converts and runs and creates badge
+* automated figure creation old repo
+
+
+
+# Aim
+* name(s) argument with defaults
+    * explicit name_I_t = "I_t"
+    * if name_I_t = None, do not export (for the optional guys)
+    * _hc_L1 to indicate hierarchical variables
+        + if hc, then we append suffixes to provided argument names
+    * last suffix `_log_` vs `_log__` from pymc3
+* model as optional argument
+* _try_ to separate hc from non hc
+* comments within functions what;s happening
+
+
+
+# Delegate
+* [ ] SM __init__.py
+* [ ] PS model.py
+* [ ] JPN compartmental_models.py
+* [ ] SM spreading_rate.py
+* [ ] JPN week_modulation.py
+* [ ] PS delay.py
+* [ ] SM likelihood.py
+* [ ] JPN utility.py
+
+* [ ] JD testing
+
+* [ ] fix examples
+* [ ] ipynb automation
+* [ ] figure automation
+"""
