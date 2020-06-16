93ec001ba3a4ece00300536e3873c5d5b90c6c3a
semohr <39738318+semohr@users.noreply.github.com>
2020-06-16 13:29:31 +0200

    Update data_collection.py
    Added low level save/load trace functions and added global data folder --> get_data_dir
    
    Important:
    Loaded trace is not a multitrace object -> creates a dict, this could be problematic if we want to use the trace in the plot functions
    Pymc3 uses [this](https://github.com/pymc-devs/pymc3/blob/master/pymc3/backends/tracetab.py) but it will be removed in the future.
    
    We should fix that before we merge it into the master.

--- covid19_inference/data_collection.py
+++ covid19_inference/data_collection.py
@@ -3,6 +3,122 @@ import numpy as np
 import os
 from covid19_inference import plotting
 
+from .data_retrieval.retrieval import set_data_dir,get_data_dir
+import datetime
+
+import logging
+log = logging.getLogger(__name__)
+
+
+# Low level save trace
+def save_trace(
+    trace,
+    dirname=None,
+    varnames=None
+    ):
+    """
+    Saves the given trace into a director inside the global data directory. One can set the global directory
+    with `set_data_dir()`, defaults to os dependent temp folder.
+
+
+    Parameters
+    ----------
+    trace : pymc3 multitrace
+        The give trace which should be saved.
+    dirname : str, optional
+        Filename for the saved files defaults to trace_backup_%timestamp%
+    varnames : arry of strings, optional
+        Defaults to all available vars
+
+
+    Returns
+    -------
+    :str
+        Full filepath to the trace files
+    """
+
+    """ Default parameters
+    """
+    
+    if dirname is None:
+        dirname = "trace_backup_"+datetime.datetime.now().strftime("%y-%m-%d-%H:%M:%S")
+
+    if varnames is None:
+        varnames = trace.varnames
+
+    """ Save data
+    """
+    # Get full path of directory
+    directory = get_data_dir()+dirname
+
+    # Check if dir exists (should never happen) and create it if it does not exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Iterate over all wanted vars in the varnames
+    for varname in varnames:
+        try:
+            # Load trace var as dataframe
+            df = pd.DataFrame(trace[str(varname)])
+        except Exception as e:
+            log.warning(f"Varname '{varname} not found in trace! [{e}]")
+        try:
+            # Save dataframe
+            f = open(directory+"/"+str(varname), "w+")
+            f.write(df.to_csv(index=False))
+            f.close()
+        except Exception as e:
+            log.warning(f"Varname '{varname}' could not be saved! [{e}]")
+
+    return directory
+
+
+def load_trace_as_dict(
+    dirname =None,
+    varnames=None
+    ):
+    """
+    Loads a trace which was earlier saved with the `save_trace` function.
+
+    Parameters
+    ----------
+    directory : str, optional
+        Name of the desired trace e.g. `trace_backup_%timestamp%`, defaults to the last saved one.
+        (Only works if the default name was used to save the trace)
+    varnames : array of strings, optional
+        If one only wants to return specific vars from the saved trace, 
+        defaults to all available vars
+    """
+
+    """ Default parameters
+    """
+    return_date = None
+    if dirname is None:
+        subfolders = [os.path.basename(f.path) for f in os.scandir(get_data_dir()) if f.is_dir() ]
+        for folder in subfolders:
+            if "trace_backup_" not in folder:
+                continue
+            # Get date of folder
+            str_time = folder[-17:]
+            timestamp = datetime.datetime.strptime(str_time, "%y-%m-%d-%H:%M:%S")
+            # If the timestamp is newer overwrite the dirname
+            if return_date is None or timestamp > return_date:
+                return_date = timestamp
+                dirname = "trace_backup_"+timestamp.strftime("%y-%m-%d-%H:%M:%S")
+        if return_date is None:
+            log.error(f"No trace saved in {get_data_dir()}!")
+
+    if varnames is None:
+        varnames = [os.path.basename(f.path) for f in os.scandir(get_data_dir()+dirname)]
+
+
+    trace = dict()
+    for varname in varnames:
+        trace[varname] = pd.read_csv(get_data_dir()+dirname+"/"+varname)
+
+    return trace
+
+
 
 def update_collection(
     country,
