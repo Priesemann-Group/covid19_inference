1195e9e857339150db261ba1ea530ff71fc370e1
jdehning <jonas.dehning@ds.mpg.de>
2022-03-24 16:02:01 +0100

    Add a 2nd step to the robust sampler burnin

--- covid19_inference/sampling.py
+++ covid19_inference/sampling.py
@@ -4,6 +4,7 @@ import warnings
 from collections import Counter
 import pickle
 import glob
+import os
 
 import pymc3 as pm
 import arviz as az
@@ -25,8 +26,8 @@ def get_start_points(trace, trace_az, frames_start=None, SD_chain_logl=2.5):
     frames_start : int
         Which frames to use for calculating the mean likelihood and its standard deviation.
         By default it is set to the last third of the tuning samples
-    SD_chain_logl : float
-        The number of standard deviations. 2.5 as default
+    SD_chain_logl : None or float
+        The number of standard deviations. 2.5 as default. If None, keep all chains
     Returns
     -------
     start_points :
@@ -42,9 +43,12 @@ def get_start_points(trace, trace_az, frames_start=None, SD_chain_logl=2.5):
     logl_mean = np.array(logl[:, frames_start:].mean(axis=1))
     logl_std = np.array(logl[:, frames_start:].std(axis=1))
     max_idx = np.argmax(logl_mean)
-    logl_thr = logl_mean[max_idx] - logl_std[max_idx] * SD_chain_logl
-    keep_chains = logl_mean >= logl_thr
-    log.info(f"Num chains kept: {np.sum(keep_chains)}/{n_chains}")
+    if SD_chain_logl is not None:
+        logl_thr = logl_mean[max_idx] - logl_std[max_idx] * SD_chain_logl
+        keep_chains = logl_mean >= logl_thr
+        log.info(f"Num chains kept: {np.sum(keep_chains)}/{n_chains}")
+    else:
+        keep_chains = np.ones_like(logl_mean).astype("bool")
 
     start_points = []
     for i, keep_chain in enumerate(keep_chains):
@@ -71,6 +75,7 @@ class Callback:
     n : int
         Save the trace every n iterations
     """
+
     def __init__(self, path="/temp", name="model", n=100):
         self.path = path
         self.name = name
@@ -85,27 +90,38 @@ class Callback:
 
     """This function is called by pymc3 every iterations
     """
+
     def __call__(self, trace, draw):
 
         # Update values
         self.lengths[draw.chain] += 1
-        
+
         # Save the trace every n iterations
         if self.lengths[draw.chain] % self.n == 0:
             self.save(trace, draw.chain)
             self.plot_logp(trace, draw.chain)
 
     def plot_logp(self, trace, chain):
-        pm_trace = pm.backends.base.MultiTrace({chain:trace}.values())
-        masked_logp = pm_trace["model_logp"][pm_trace["model_logp"]!=0]
-
-        self.ax.plot(np.arange(self.lengths[chain]-len(masked_logp),self.lengths[chain]),masked_logp)
-        self.fig.savefig(f"{self.path}/{self.name}_logp.png")
+        pm_trace = pm.backends.base.MultiTrace({chain: trace}.values())
+        masked_logp = pm_trace["model_logp"][pm_trace["model_logp"] != 0]
+        try:
+            self.ax.plot(
+                np.arange(self.lengths[chain] - len(masked_logp), self.lengths[chain]),
+                masked_logp,
+            )
+            self.fig.savefig(os.path.join(self.path, self.name) + "_logp.png")
+        except:
+            log.warning(f"Could not save {os.path.join(self.path, self.name)}_logp.png")
 
     def save(self, trace, chain):
-        with open(f"{self.path}/{self.name}_{chain}.pkl", "wb") as f:
-            trace.chain = chain
-            pickle.dump(trace, f)
+        try:
+            with open(os.path.join(self.path, self.name) + f"_{chain}.pkl", "wb") as f:
+                trace.chain = chain
+                pickle.dump(trace, f)
+        except:
+            log.warning(
+                f"Could not save {os.path.join(self.path, self.name)}_{chain}.pkl"
+            )
 
     def load_all(self):
         files = glob.glob(f"{self.path}/{self.name}_*.pkl")
@@ -117,20 +133,84 @@ class Callback:
         return az.from_pymc3(pm.backends.base.MultiTrace(traces.values()))
 
 
+def burn_in(
+    model,
+    n_tune,
+    n_chains_burn_in,
+    n_chains_final,
+    step_method,
+    start_points=None,
+    callback=None,
+    args_start_points=None,
+    sample_kwargs=None,
+):
+    if sample_kwargs is None:
+        sample_kwargs = {}
+    i = 0
+    while i < 50:
+        try:
+            trace_tuning = pm.sample(
+                model=model,
+                tune=n_tune,
+                draws=0,
+                start=start_points,
+                chains=n_chains_burn_in,
+                return_inferencedata=False,
+                discard_tuned_samples=False,
+                step=step_method,
+                callback=callback,
+                **sample_kwargs,
+            )
+        except RuntimeError as error:
+            if i < 10:
+                i += 1
+                log.warning(
+                    f"Tuning lead to a nan error in one chain, "
+                    f"trying again (try no {i})."
+                )
+                continue
+
+            else:
+                raise error
+        i = 1000
+
+    trace_tuning_az = az.from_pymc3(trace_tuning, model=model, save_warmup=True)
+    if args_start_points is None:
+        args_start_points = {}
+    start_points, logl_starting_points = get_start_points(
+        trace_tuning, trace_tuning_az, SD_chain_logl=None, **args_start_points
+    )
+    num_start_points = len(start_points)
+
+    if num_start_points < n_chains_final:
+        log.warning(
+            "Not enough chains converged to minimum, we recommend increasing the number of tuning chains"
+        )
+        start_points = random.choices(start_points, k=n_chains_final)
+    elif num_start_points > n_chains_final:
+        p = np.exp(logl_starting_points - max(logl_starting_points))
+        start_points = np.random.choice(
+            start_points, size=n_chains_final, p=p / np.sum(p), replace=False,
+        )
+    return start_points, trace_tuning_az
+
+
 def robust_sample(
     model,
     tune,
     draws,
-    tuning_chains,
     final_chains,
-    return_tuning=False,
+    burnin_chains,
+    burnin_draws=None,
+    burnin_chains_2nd=None,
+    burnin_draws_2nd=None,
     args_start_points=None,
-    tune_2nd=None,
     callback=None,
+    sample_kwargs=None,
     **kwargs,
 ):
     r"""
-    Samples the model by starting more chains than needed (tuning chains) and using only
+    Samples the model by starting more chains than needed (burn-in chains) and using only
     a reduced number final_chains for the final sampling. The final chains are randomly
     chosen (without replacement) weighted by their likelihood.
     Parameters
@@ -141,14 +221,26 @@ def robust_sample(
         Number of tuning samples
     draws : int
         Number of final samples
-    tuning_chains : int
-        Number of tuning chains
     final_chains : int
         Number of draw chains
+    burnin_chains : int
+        Number of chains used during burn-in, recommended to use about 2-3 time more than
+        the number of final_chains
+    burnin_draws : int
+        Length of the burn-in period, can be fairly short, on the order of a few hundreds
+        draws. By default it set to tune//2
+    burnin_chains_2nd : int
+        If not None, use a two-stage burn-in period, reducing the number of chains each time,
+        Therefore, it should be less than burnin_chains and more than final_chains:
+        burnin_chains > burnin_chains_2nd > final_chains
+    burnin_draws_2nd : int
+        Length of the second burn-in period. By default it set burnin_draws
     args_start_points : dict
         Arguments passed to `get_start_points`
     tune_2nd : int
         If set, use different number of tuning samples for the second tuning
+    sample_kwargs:
+        Arguments passed to pm.sample
     **kwargs :
         Arguments passed to the nuts step function.
 
@@ -158,6 +250,21 @@ def robust_sample(
     trace_az : trace as arviz object
 
     """
+
+    burnin_chains_2nd_to_compare = (
+        burnin_chains_2nd if burnin_chains_2nd is not None else final_chains + 0.5
+    )
+    if not burnin_chains > burnin_chains_2nd_to_compare > final_chains:
+        raise RuntimeError(
+            "The number of chains should decrease for good sampline: "
+            "burnin_chains > burnin_chains_2nd > final_chains"
+        )
+
+    if burnin_draws is None:
+        burnin_draws = tune // 2
+    if burnin_draws_2nd is None and burnin_chains_2nd is not None:
+        burnin_draws_2nd = burnin_draws
+
     with warnings.catch_warnings():
         warnings.filterwarnings(
             "ignore", message=".*invalid value encountered in double_scalars.*"
@@ -176,58 +283,36 @@ def robust_sample(
 
         # Create nuts step method class to reuse for tuning and later sampling
         default_nuts = pm.NUTS(model=model, **kwargs)
+        if sample_kwargs is None:
+            sample_kwargs = {}
 
-        i = 0
-        while i < 50:
-            try:
-                trace_tuning = pm.sample(
-                    model=model,
-                    tune=tune,
-                    draws=0,
-                    chains=tuning_chains,
-                    return_inferencedata=False,
-                    discard_tuned_samples=False,
-                    step=default_nuts,
-                    callback=callback,
-                )
-            except RuntimeError as error:
-                if i < 10:
-                    i += 1
-                    log.warning(
-                        f"Tuning lead to a nan error in one chain, "
-                        f"trying again (try no {i})."
-                    )
-                    continue
-
-                else:
-                    raise error
-            i = 1000
-
-        trace_tuning_az = az.from_pymc3(trace_tuning, model=model, save_warmup=True)
-        if args_start_points is None:
-            args_start_points = {}
-        start_points, logl_starting_points = get_start_points(
-            trace_tuning, trace_tuning_az, **args_start_points
+        ## Burn-in
+        start_points, trace_burn_in_1_az = burn_in(
+            model,
+            n_tune=burnin_draws,
+            n_chains_burn_in=burnin_chains,
+            n_chains_final=final_chains
+            if burnin_chains_2nd is None
+            else burnin_chains_2nd,
+            step_method=default_nuts,
+            args_start_points=args_start_points,
+            sample_kwargs=sample_kwargs,
         )
-        num_start_points = len(start_points)
-
-        if num_start_points < final_chains:
-            log.warning(
-                "Not enough chains converged to minimum, we recommend increasing the number of tuning chains"
-            )
-            start_points = random.choices(start_points, k=final_chains)
-        elif num_start_points > final_chains:
-            p = np.exp(logl_starting_points - max(logl_starting_points))
-            start_points = np.random.choice(
-                start_points,
-                size=final_chains,
-                p=p / np.sum(p),
-                replace=False,
+        if burnin_chains_2nd is not None:
+            start_points, trace_burn_in_2_az = burn_in(
+                model,
+                n_tune=burnin_draws_2nd,
+                n_chains_burn_in=burnin_chains_2nd,
+                n_chains_final=final_chains,
+                start_points=start_points,
+                step_method=default_nuts,
+                args_start_points=args_start_points,
+                sample_kwargs=sample_kwargs,
             )
 
         trace = pm.sample(
             model=model,
-            tune=tune_2nd if tune_2nd is not None else tune,
+            tune=tune,
             draws=draws,
             chains=final_chains,
             start=start_points,
@@ -235,9 +320,26 @@ def robust_sample(
             discard_tuned_samples=False,
             step=default_nuts,
             callback=callback,
+            **sample_kwargs,
         )
         trace_az = az.from_pymc3(trace, model=model, save_warmup=True)
-    if return_tuning:
-        return trace, trace_az, trace_tuning, trace_tuning_az
-    else:
-        return trace, trace_az
+
+        def append_burn_in_samples(trace, trace_burn_in, num):
+            new_names = [
+                f"burn-in-{num}_" + n.replace("warmup_", "")
+                for n in trace_burn_in.groups()
+            ]
+            with warnings.catch_warnings():
+                warnings.filterwarnings(
+                    "ignore", message="The group .* is not defined in the .* scheme",
+                )
+                trace.add_groups(
+                    {n: o for n, o in zip(new_names, trace_burn_in.values())}
+                )
+            return trace
+
+        trace_az = append_burn_in_samples(trace_az, trace_burn_in_1_az, 1)
+        if burnin_chains_2nd is not None:
+            trace_az = append_burn_in_samples(trace_az, trace_burn_in_2_az, 2)
+
+    return trace, trace_az
